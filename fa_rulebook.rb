78f3f35511eaab6f94593fd83291d7d6e655e686
Lu Liu <luliu27@gmail.com>
2015-09-06 04:13:54 -0400

    add nondeterministic finite automata simulation

--- fa_rulebook.rb
+++ fa_rulebook.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class FARule < Struct.new(:state, :char, :next_state)
   def applies_to?(state, char)
     self.state == state && self.char == char
@@ -54,3 +56,60 @@ class DFADesign < Struct.new(:start_state, :accept_states, :rule_book)
     to_dfa.tap {|dfa| dfa.read_string(string)}.accepting?
   end
 end
+
+class NFARulebook < Struct.new(:rules)
+  def next_states(states, char)
+    states.flat_map { |state|
+      follow_rules_for(state, char)
+    }.to_set
+  end
+
+  def follow_rules_for(state, char)
+    rules_for(state, char).map(&:follow)
+  end
+
+  def rules_for(state, char)
+    rules.select { |rule| rule.applies_to?(state, char) }
+  end
+end
+
+class NFA < Struct.new(:current_states, :accept_states, :rule_book)
+  def accepting?
+    (current_states & accept_states).any?
+  end
+
+  def read_char(char)
+    self.current_states = rule_book.next_states(current_states, char)
+  end
+
+  def read_string(string)
+    string.chars.each do |char|
+      read_char(char)
+    end
+  end
+end
+
+class NFADesign < Struct.new(:start_state, :accept_states, :rule_book)
+  def to_nfa
+    NFA.new(Set[start_state], accept_states, rule_book)
+  end
+
+  def accepts?(string)
+    to_nfa.tap {|nfa| nfa.read_string(string)}.accepting?
+  end
+end
+
+# example -- third-from-last character is 'b'
+# rulebook = NFARulebook.new([
+#       FARule.new(1, 'a', 1),
+#       FARule.new(1, 'b', 1),
+#       FARule.new(1, 'b', 2),
+#       FARule.new(2, 'a', 3),
+#       FARule.new(2, 'b', 3),
+#       FARule.new(3, 'a', 4),
+#       FARule.new(3, 'b', 4)])
+# nfa = NFADesign.new(1, [4], rulebook)
+# nfa.accepts?('ba')
+# -> false
+# nfa.accepts?('baa')
+# -> true
